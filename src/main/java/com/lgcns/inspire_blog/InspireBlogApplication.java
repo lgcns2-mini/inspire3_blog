e19d627f96e704e82dd407bc27709fd865feb223
aaahyunseo <hs030608@naver.com>
2025-10-23 10:35:40 +0900

    init : blog initialization

---
+++ src/main/java/com/lgcns/inspire_blog/InspireBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspireBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/inspire_blog/InspireBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
